a4f4753b04122d06b7401290e2d88ecb980f3099
Serezha <102390317+SergeiTregub@users.noreply.github.com>
2024-02-04 13:57:20 +0300

    Create RPi4Router.sh

---
+++ RPi4Router.sh
@@ -0,0 +1,7 @@
+echo Welcome to RaspAP Installer Script.
+echo SergeiTregub on GitHub.
+sleep 3
+echo Now installing RaspAP via curl
+curl -sL https://install.raspap.com | bash
+echo Installation completed
+echo Please reboot
